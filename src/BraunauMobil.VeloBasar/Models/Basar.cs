a4d2e91c4b30eda76c4265c587b94d9eb0dabf4a
Alexander Kastler <github@xaka.eu>
2019-05-31 17:23:45 +0200

    Add Basar model

---
+++ src/BraunauMobil.VeloBasar/Models/Basar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BraunauMobil.VeloBasar.Models
+{
+    public class Basar
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal ProductCommission { get; set; }
+
+        public decimal ProductDiscount { get; set; }
+
+        public decimal SellerDiscount { get; set; }
+    }
+}
