9c528634127d1bc75992957518b41afa81d01cf7
Alexander Kastler <github@xaka.eu>
2019-08-10 13:50:15 +0200

    Introduce BasarPageModel.GetRoute

--- src/BraunauMobil.VeloBasar/BasarPageModel.cs
+++ src/BraunauMobil.VeloBasar/BasarPageModel.cs
@@ -3,6 +3,7 @@ using BraunauMobil.VeloBasar.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace BraunauMobil.VeloBasar
@@ -19,6 +20,10 @@ namespace BraunauMobil.VeloBasar
         [BindProperty]
         public Basar Basar { get; set; }
 
+        protected void DecorateRoute(IDictionary<string, string> route)
+        {
+        }
+
         protected async Task LoadBasarAsync(int? basarId)
         {
 #if DEBUG
@@ -28,6 +33,17 @@ namespace BraunauMobil.VeloBasar
             {
                 Basar = await Context.Basar.FirstOrDefaultAsync(m => m.Id == basarId);
             }
+        }
+
+        public virtual IDictionary<string, string> GetRoute()
+        {
+            var route = new Dictionary<string, string>();
+            if (Basar != null)
+            {
+                route.Add("basarId", Basar.Id.ToString());
+            }
+            DecorateRoute(route);
+            return route;
         }
     }
 }
