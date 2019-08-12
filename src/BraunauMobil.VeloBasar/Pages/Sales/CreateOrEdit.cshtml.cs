9c5ce24c7d536c50bf601969ec682f1e0d440d8f
Alexander Kastler <github@xaka.eu>
2019-08-12 16:21:02 +0200

    Add error handling to CreateOrEdit Sale page

--- src/BraunauMobil.VeloBasar/Pages/Sales/CreateOrEdit.cshtml.cs
+++ src/BraunauMobil.VeloBasar/Pages/Sales/CreateOrEdit.cshtml.cs
@@ -5,6 +5,7 @@ using BraunauMobil.VeloBasar.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BraunauMobil.VeloBasar.Pages.Sales
 {
@@ -14,15 +15,18 @@ namespace BraunauMobil.VeloBasar.Pages.Sales
         {
         }
 
+        public string ErrorText { get; set; }
+
         public Sale Sale { get; set; }
 
         [BindProperty]
         public IList<Product> Products { get; set; }
 
         [BindProperty]
+        [Required]
         public int ProductId { get; set; }
 
-        public async Task OnGetAsync(int basarId, int? saleId)
+        public async Task OnGetAsync(int basarId, int? saleId, string errorText)
         {
             await LoadBasarAsync(basarId);
 
@@ -35,22 +39,28 @@ namespace BraunauMobil.VeloBasar.Pages.Sales
             {
                 Products = new PaginatedList<Product>();
             }
+
+            ErrorText = errorText;
         }
 
         public async Task<IActionResult> OnPostAsync(int basarId, int? saleId)
         {
+            TryValidateModel(ProductId);
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
             await LoadBasarAsync(basarId);
 
             var product = await Context.Product.FirstOrDefaultAsync(p => p.Id == ProductId);
             if (product == null)
             {
-                //  @todo!!
-                return Page();
+                return RedirectToPage("/Sales/CreateOrEdit", new { basarId, saleId, errorText = $"Es wurde kein Produkt mit der id {ProductId} gefunden" });
             }
             if (product.Status != ProductStatus.Available)
             {
-                //  @todo
-                return Page();
+                return RedirectToPage("/Sales/CreateOrEdit", new { basarId, saleId, errorText = $"Das Produkt {product.Brand} {product.Type} mit der id {ProductId} ist nicht mehr verfügbar." });
             }
 
             var sale = await Context.AddProductToSaleAsync(basarId, saleId, product);
