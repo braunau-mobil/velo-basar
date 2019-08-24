848acf7a1e2b8f9eb8da84d15fedad087585cac9
Alexander Kastler <github@xaka.eu>
2019-08-24 20:18:46 +0200

    Introduce ListViewModel

---
+++ src/BraunauMobil.VeloBasar/ListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using BraunauMobil.VeloBasar.Models;
+using System.Collections.Generic;
+
+namespace BraunauMobil.VeloBasar
+{
+    public class ListViewModel<T> : BasarViewModel
+    {
+        public ListViewModel(Basar basar, IEnumerable<T> list) : base(basar)
+        {
+            List = list;
+        }
+
+        public IEnumerable<T> List
+        {
+            get;
+            set;
+        }
+
+        public IDictionary<string, string> GetItemRoute(Product product)
+        {
+            var route = GetRoute();
+            route.Add("productId", product.Id.ToString());
+            return route;
+        }
+    }
+}
