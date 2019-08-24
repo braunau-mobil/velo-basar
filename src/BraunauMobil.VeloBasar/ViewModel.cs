b8520ab7da7863d85b57f354a2749173fc9242d4
Alexander Kastler <github@xaka.eu>
2019-08-24 20:20:00 +0200

    Introduce ViewModel

---
+++ src/BraunauMobil.VeloBasar/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BraunauMobil.VeloBasar.Models;
+
+namespace BraunauMobil.VeloBasar
+{
+    public class ViewModel<T> : BasarViewModel
+    {
+        public ViewModel(Basar basar, T value) : base(basar)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+    }
+}
