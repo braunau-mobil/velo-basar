08d4c5d0ec1e9cbc7642253faced71d6d38f6935
Alexander Kastler <github@xaka.eu>
2019-08-24 20:18:27 +0200

    Introduce BasarViewModel

---
+++ src/BraunauMobil.VeloBasar/BasarViewModel.cs
@@ -0,0 +1,27 @@
+﻿using BraunauMobil.VeloBasar.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BraunauMobil.VeloBasar
+{
+    public class BasarViewModel
+    {
+        public BasarViewModel(Basar basar)
+        {
+            Basar = basar;
+        }
+
+        [BindProperty]
+        public Basar Basar { get; set; }
+
+        public virtual IDictionary<string, string> GetRoute()
+        {
+            var route = new Dictionary<string, string>();
+            if (Basar != null)
+            {
+                route.Add("basarId", Basar.Id.ToString());
+            }
+            return route;
+        }
+    }
+}
