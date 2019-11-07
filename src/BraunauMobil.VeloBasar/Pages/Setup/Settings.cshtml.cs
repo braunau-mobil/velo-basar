d986c7bd589d2e049ef8cdcc43dc8071d90084a0
Alexander Kastler <github@xaka.eu>
2019-11-07 20:41:24 +0100

    #62 Introduce Settings

---
+++ src/BraunauMobil.VeloBasar/Pages/Setup/Settings.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using BraunauMobil.VeloBasar.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BraunauMobil.VeloBasar.Pages.Setup
+{
+    public class SettingsModel : PageModel
+    {
+        private readonly VeloBasarContext _context;
+
+        public SettingsModel(VeloBasarContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        [Display(Name = "Aktiver Basar")]
+        public int ActiveBasarId { get; set; }
+
+        public void OnGet()
+        {
+            ViewData["Basars"] = new SelectList(_context.Basar, "Id", "Name");
+            ActiveBasarId = _context.Settings.ActiveBasar.Id;
+        }
+        public async Task OnPostAsync()
+        {
+            ViewData["Basars"] = new SelectList(_context.Basar, "Id", "Name");
+            _context.Settings.ActiveBasarId = ActiveBasarId;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
