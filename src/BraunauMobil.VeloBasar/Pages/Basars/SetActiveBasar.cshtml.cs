521c814eb6721620c7ca7e4da7bad2a8a0cd5be4
Alexander Kastler <github@xaka.eu>
2019-11-07 20:34:36 +0100

    #62 Introduce drop down for basar selection

---
+++ src/BraunauMobil.VeloBasar/Pages/Basars/SetActiveBasar.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using BraunauMobil.VeloBasar.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BraunauMobil.VeloBasar.Pages.Basars
+{
+    [Authorize]
+    public class SetActiveBasarModel : PageModel
+    {
+        private readonly VeloBasarContext _context;
+
+        public SetActiveBasarModel(VeloBasarContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int basarId)
+        {
+            _context.Settings.ActiveBasarId = basarId;
+            await _context.SaveChangesAsync();
+            return Redirect(Request.Headers["Referer"].ToString());
+        }
+    }
+}
