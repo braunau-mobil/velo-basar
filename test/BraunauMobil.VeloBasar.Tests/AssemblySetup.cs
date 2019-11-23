43e97fd19ebcb54cf596107a5f52ba610c0c6aec
Alexander Kastler <github@xaka.eu>
2019-11-23 19:18:22 +0100

    #20 Add some tests

---
+++ test/BraunauMobil.VeloBasar.Tests/AssemblySetup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+
+namespace BraunauMobil.VeloBasar.Tests
+{
+    [TestClass]
+    public static class AssemblySetup
+    {
+        [AssemblyInitialize]
+        public static void Init(TestContext testContext)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.CurrentUICulture = CultureInfo.InvariantCulture;
+        }
+    }
+}
---
+++ test/BraunauMobil.VeloBasar.Tests/Models/BasarTests/GetDateText.cs
@@ -0,0 +1,28 @@
+﻿using BraunauMobil.VeloBasar.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BraunauMobil.VeloBasar.Tests.Models.BasarTests
+{
+    [TestClass]
+    public class GetDateText
+    {
+        [TestMethod]
+        public void Empty()
+        {
+            var basar = new Basar();
+            Assert.AreEqual("01.January.0001", basar.GetDateText());
+        }
+        [TestMethod]
+        public void FirstContact()
+        {
+            var basar = new Basar
+            {
+                Date = new DateTime(2063, 04, 05)
+            };
+            Assert.AreEqual("05.April.2063", basar.GetDateText());
+        }
+    }
+}
