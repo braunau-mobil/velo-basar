3dd1cadd323e94cf60559bb4f6568bf4b5a42e04
Alexander Kastler <github@xaka.eu>
2019-11-30 22:00:29 +0100

    Add DropDatabase to DevTools

---
+++ src/BraunauMobil.VeloBasar/Pages/DevTools/DropDatabase.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BraunauMobil.VeloBasar.Pages.DevTools
+{
+    public class DropDatabaseModel : PageModel
+    {
+        private readonly IVeloContext _context;
+
+        public DropDatabaseModel(IVeloContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (!_context.Configuration.DevToolsEnabled())
+            {
+                return Unauthorized();
+            }
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!_context.Configuration.DevToolsEnabled())
+            {
+                return Unauthorized();
+            }
+            await _context.Db.Database.EnsureDeletedAsync();
+            await _context.Db.SaveChangesAsync();
+            return this.RedirectToPage<IndexModel>();
+        }
+    }
+}
