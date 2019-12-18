9db083330be815c5ec32db71ccaa4c2f3a299cca
Alexander Kastler <github@xaka.eu>
2019-12-18 23:11:43 +0100

    #20 Add Tests for Seller

---
+++ test/BraunauMobil.VeloBasar.Tests/Models/SellerTests/GetBigAddressText.cs
@@ -0,0 +1,32 @@
+﻿using BraunauMobil.VeloBasar.Models;
+using Xunit;
+
+namespace BraunauMobil.VeloBasar.Tests.Models.SellerTests
+{
+    public class GetBigAddressText
+    {
+        [Fact]
+        public void AllEmpty()
+        {
+            var seller = new Seller();
+            Assert.Equal(" \r\n\r\n \r\n",  seller.GetBigAddressText());
+        }
+        [Fact]
+        public void AllSet()
+        {
+            var seller = new Seller
+            {
+                City = "Hopfenhausen",
+                Country = new Country
+                {
+                    Name = "Gerstania",
+                },
+                FirstName = "Ione",
+                LastName = "Bierfrau",
+                Street = "Rebengasse",
+                ZIP = "1234"
+            };
+            Assert.Equal("Ione Bierfrau\r\nRebengasse\r\n1234 Hopfenhausen\r\n", seller.GetBigAddressText());
+        }
+    }
+}
---
+++ test/BraunauMobil.VeloBasar.Tests/Models/SellerTests/GetIdText.cs
@@ -0,0 +1,24 @@
+﻿using BraunauMobil.VeloBasar.Models;
+using Xunit;
+
+namespace BraunauMobil.VeloBasar.Tests.Models.SellerTests
+{
+    public class GetIdText
+    {
+        [Fact]
+        public void Emtpy()
+        {
+            var seller = new Seller();
+            Assert.Equal("Verk.-ID: 0", seller.GetIdText(TestUtils.CreateLocalizer()));
+        }
+        [Fact]
+        public void NotEmtpy()
+        {
+            var seller = new Seller
+            {
+                Id = 666
+            };
+            Assert.Equal("Verk.-ID: 666", seller.GetIdText(TestUtils.CreateLocalizer()));
+        }
+    }
+}
