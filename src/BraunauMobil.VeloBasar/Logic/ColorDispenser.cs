59724ec02e346ca71e1527a117f1b619caa2ba9f
Alexander Kastler <github@xaka.eu>
2020-01-25 22:53:40 +0100

    Improve Colors

--- src/BraunauMobil.VeloBasar/Logic/ColorDispenser.cs
+++ src/BraunauMobil.VeloBasar/Logic/ColorDispenser.cs
@@ -9,13 +9,14 @@ namespace BraunauMobil.VeloBasar.Logic
 
         public ColorDispenser()
         {
-            _colors.Enqueue(Color.FromRgb(255, 99, 132));
-            _colors.Enqueue(Color.FromRgb(255, 159, 64));
-            _colors.Enqueue(Color.FromRgb(255, 205, 86));
-            _colors.Enqueue(Color.FromRgb(75, 192, 192));
-            _colors.Enqueue(Color.FromRgb(54, 162, 235));
-            _colors.Enqueue(Color.FromRgb(153, 102, 255));
-            _colors.Enqueue(Color.FromRgb(201, 203, 207));
+            _colors.Enqueue(Color.FromRgb(0x00, 0x7b, 0xff));
+            _colors.Enqueue(Color.FromRgb(0x6c, 0x75, 0x7d));
+            _colors.Enqueue(Color.FromRgb(0x28, 0xa7, 0x45));
+            _colors.Enqueue(Color.FromRgb(0x17, 0xa2, 0xb8));
+            _colors.Enqueue(Color.FromRgb(0xff, 0xc1, 0x07));
+            _colors.Enqueue(Color.FromRgb(0xdc, 0x35, 0x45));
+            _colors.Enqueue(Color.FromRgb(0xf8, 0xf9, 0xfa));
+            _colors.Enqueue(Color.FromRgb(0x34, 0x3a, 0x40));
         }
 
         public Color Next()
