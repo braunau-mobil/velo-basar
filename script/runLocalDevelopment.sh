830fa3e8a11755bc5a7705eb96b1d835604a6890
Alexander Kastler <github@xaka.eu>
2020-02-16 12:22:51 +0100

    Add script to run local on unix systems

---
+++ script/runLocalDevelopment.sh
@@ -0,0 +1,4 @@
+cd ..
+ASPNETCORE_ENVIRONMENT=Development
+export ASPNETCORE_ENVIRONMENT
+dotnet run -c Debug --project ./src/BraunauMobil.VeloBasar/BraunauMobil.VeloBasar.csproj --urls https://*:5000
