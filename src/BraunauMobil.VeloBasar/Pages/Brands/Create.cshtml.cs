9fa1000ddcf1bec80ce2470cb9de685791224284
Alexander Kastler <github@xaka.eu>
2021-03-17 12:44:55 +0100

    Remove localizer from generic model

--- src/BraunauMobil.VeloBasar/Pages/Brands/Create.cshtml.cs
+++ src/BraunauMobil.VeloBasar/Pages/Brands/Create.cshtml.cs
@@ -1,14 +1,12 @@
 ﻿using BraunauMobil.VeloBasar.Logic.Generic;
 using BraunauMobil.VeloBasar.Models;
 using BraunauMobil.VeloBasar.Pages.Generic;
-using BraunauMobil.VeloBasar.Resources;
-using Microsoft.AspNetCore.Mvc.Localization;
 
 namespace BraunauMobil.VeloBasar.Pages.Brands
 {
     public class CreateModel : CreateModel<Brand, ListModel>
     {
-        public CreateModel(ICrudContext<Brand> context, IHtmlLocalizer<SharedResource> localizer) : base(context, localizer)
+        public CreateModel(ICrudContext<Brand> context) : base(context)
         {
             Title = "Marke hinzufügen";
         }
--- src/BraunauMobil.VeloBasar/Pages/Generic/CreateModel.cs
+++ src/BraunauMobil.VeloBasar/Pages/Generic/CreateModel.cs
@@ -1,8 +1,6 @@
 ﻿using BraunauMobil.VeloBasar.Logic.Generic;
 using BraunauMobil.VeloBasar.Models.Interfaces;
-using BraunauMobil.VeloBasar.Resources;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Localization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
 
@@ -11,12 +9,10 @@ namespace BraunauMobil.VeloBasar.Pages.Generic
     public abstract class CreateModel<TModel, TListPageModel> : PageModel where TModel : IModel, new() where TListPageModel : PageModel
     {
         private readonly ICrudContext<TModel> _context;
-        private readonly IHtmlLocalizer<SharedResource> _localizer;
 
-        public CreateModel(ICrudContext<TModel> context, IHtmlLocalizer<SharedResource> localizer)
+        public CreateModel(ICrudContext<TModel> context)
         {
             _context = context;
-            _localizer = localizer;
         }
 
         [BindProperty]
@@ -25,7 +21,6 @@ namespace BraunauMobil.VeloBasar.Pages.Generic
 
         public IActionResult OnGet()
         {
-            ViewData["Title"] = _localizer[Title];
             Item = new TModel();
             return Page();
         }
