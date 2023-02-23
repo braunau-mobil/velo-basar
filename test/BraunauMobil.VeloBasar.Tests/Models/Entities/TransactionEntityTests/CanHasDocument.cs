86e5f599f1fcf1bf19c3b60891f3754955f8fce7
Alexander Kastler <github@xaka.eu>
2023-02-23 15:27:12 +0100

    Add Test for CanHasDocument

---
+++ test/BraunauMobil.VeloBasar.Tests/Models/Entities/TransactionEntityTests/CanHasDocument.cs
@@ -0,0 +1,32 @@
+﻿namespace BraunauMobil.VeloBasar.Tests.Models.Entities.TransactionEntityTests;
+
+public class CanHasDocument
+{
+    [Theory]
+    [InlineData(TransactionType.Acceptance)]
+    [InlineData(TransactionType.Sale)]
+    [InlineData(TransactionType.Settlement)]
+    public void Yes(TransactionType transactionType)
+    {
+        var transaction = new TransactionEntity
+        {
+            Type = transactionType
+        };
+        transaction.CanHasDocument
+            .Should().BeTrue();
+    }
+    [Theory]
+    [InlineData(TransactionType.Cancellation)]
+    [InlineData(TransactionType.Lock)]
+    [InlineData(TransactionType.SetLost)]
+    [InlineData(TransactionType.Unlock)]
+    public void No(TransactionType transactionType)
+    {
+        var transaction = new TransactionEntity
+        {
+            Type = transactionType
+        };
+        transaction.CanHasDocument
+            .Should().BeFalse();
+    }
+}
