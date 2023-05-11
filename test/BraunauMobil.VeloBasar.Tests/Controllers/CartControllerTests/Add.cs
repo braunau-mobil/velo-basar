d0aabe358fa7816f70b02fbb293028cd97b933d6
Alexander Kastler <github@xaka.eu>
2023-05-11 20:57:10 +0200

    Add tests for CartController.Add

---
+++ test/BraunauMobil.VeloBasar.Tests/Controllers/CartControllerTests/Add.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BraunauMobil.VeloBasar.Tests.Controllers.CartControllerTests;
+
+public class Add
+    : TestBase
+{
+    [Theory]
+    [AutoData]
+    public async Task InvalidCartModel_IsNotAddedToCookieAndReturnsView(CartModel cartModel, IList<int> cart, IReadOnlyList<ProductEntity> products)
+    {
+        //  Arrange
+        Cookie.Setup(_ => _.GetCart())
+            .Returns(cart);
+        ProductService.Setup(_ => _.GetManyAsync(cart))
+            .ReturnsAsync(products);
+
+        //  Act
+        IActionResult result = await Sut.Add(cartModel);
+
+        // Assert
+        result.Should().NotBeNull();
+
+        ViewResult viewResult = result.Should().BeOfType<ViewResult>().Subject;
+        viewResult.ViewData.ModelState.ErrorCount.Should().Be(1);
+
+        viewResult.Model.Should().NotBeNull();
+        viewResult.Model.Should().Be(cartModel);
+
+        Cookie.Verify(_ => _.GetCart(), Times.Once());
+        ProductService.Verify(_ => _.GetManyAsync(cart), Times.Once());
+        ProductService.Verify(_ => _.FindAsync(cartModel.ProductId), Times.Once());
+        VerifyNoOtherCalls();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task ValidCartModel_IsAddedToCookieAndReturnsView(CartModel cartModel, ProductEntity product, IReadOnlyList<ProductEntity> products)
+    {
+        //  Arrange
+        Mock<IList<int>> cartMock = new();
+        Cookie.Setup(_ => _.GetCart())
+            .Returns(cartMock.Object);
+        ProductService.Setup(_ => _.GetManyAsync(cartMock.Object))
+            .ReturnsAsync(products);
+        product.StorageState = StorageState.Available;
+        product.ValueState = ValueState.NotSettled;
+        ProductService.Setup(_ => _.FindAsync(cartModel.ProductId))
+            .ReturnsAsync(product);
+
+        //  Act
+        IActionResult result = await Sut.Add(cartModel);
+
+        // Assert
+        result.Should().NotBeNull();
+
+        ViewResult viewResult = result.Should().BeOfType<ViewResult>().Subject;
+        viewResult.ViewData.ModelState.ErrorCount.Should().Be(0);
+
+        viewResult.Model.Should().NotBeNull();
+        viewResult.Model.Should().Be(cartModel);
+
+        cartMock.Verify(_ => _.Add(cartModel.ProductId));
+        cartMock.VerifyNoOtherCalls();
+        Cookie.Verify(_ => _.GetCart(), Times.Once());
+        Cookie.Verify(_ => _.SetCart(It.IsAny<IList<int>>()), Times.Once());
+        ProductService.Verify(_ => _.GetManyAsync(cartMock.Object), Times.Once());
+        ProductService.Verify(_ => _.FindAsync(cartModel.ProductId), Times.Once());
+        VerifyNoOtherCalls();
+    }
+}
---
+++ test/BraunauMobil.VeloBasar.Tests/Controllers/CartControllerTests/TestBase.cs
@@ -0,0 +1,51 @@
+﻿using BraunauMobil.VeloBasar.BusinessLogic;
+using BraunauMobil.VeloBasar.Controllers;
+using BraunauMobil.VeloBasar.Cookies;
+using BraunauMobil.VeloBasar.Routing;
+using Microsoft.Extensions.Localization;
+
+namespace BraunauMobil.VeloBasar.Tests.Controllers.CartControllerTests;
+
+public class TestBase
+{
+    public TestBase()
+    {
+        Validator = new(ProductService.Object, TransactionService.Object, Router.Object, Localizer);
+        Sut = new CartController(ProductService.Object, TransactionService.Object, Router.Object, Validator, Cookie.Object);
+
+        Router.Setup(_ => _.Cancel)
+            .Returns(CancelRouter.Object);
+        Router.Setup(_ => _.Transaction)
+            .Returns(TransactionRouter.Object);
+    }
+
+    public void VerifyNoOtherCalls()
+    {
+        CancelRouter.VerifyNoOtherCalls();
+        Cookie.VerifyNoOtherCalls();
+        ProductService.VerifyNoOtherCalls();
+        TransactionRouter.VerifyNoOtherCalls();
+        TransactionService.VerifyNoOtherCalls();
+    }
+
+    protected Mock<ICancelRouter> CancelRouter { get; } = new();
+
+    protected Mock<ICartCookie> Cookie { get; } = new();
+
+    protected Fixture Fixture { get; } = new ();
+
+    protected IStringLocalizer<SharedResources> Localizer { get; } = Helpers.CreateActualLocalizer();
+
+    protected Mock<IProductService> ProductService { get; } = new ();
+
+    protected Mock<IVeloRouter> Router { get; } = new();
+
+    protected CartController Sut { get; }
+
+    protected Mock<ITransactionRouter> TransactionRouter { get; } = new();
+
+    protected Mock<ITransactionService> TransactionService { get; } = new ();
+    
+    protected CartModelValidator Validator { get; }
+
+}
