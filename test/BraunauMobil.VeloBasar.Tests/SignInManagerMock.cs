84c62e9b3c554f3fa6118731761481d9cb6b28d3
Alexander Kastler <github@xaka.eu>
2023-05-18 13:58:21 +0200

    Add SignInManagerMock and UserManagerMock

---
+++ test/BraunauMobil.VeloBasar.Tests/SignInManagerMock.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace BraunauMobil.VeloBasar.Tests;
+
+public class SignInManagerMock
+    : SignInManager<IdentityUser>
+{
+    public SignInManagerMock()
+        : base(
+            new UserManagerMock(),
+            Mock.Of<IHttpContextAccessor>(),
+            Mock.Of<IUserClaimsPrincipalFactory<IdentityUser>>(),
+            Mock.Of<IOptions<IdentityOptions>>(),
+            Mock.Of<ILogger<SignInManager<IdentityUser>>>(),
+            Mock.Of<IAuthenticationSchemeProvider>(),
+            Mock.Of<IUserConfirmation<IdentityUser>>())
+    {
+    }
+}
---
+++ test/BraunauMobil.VeloBasar.Tests/UserManagerMock.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace BraunauMobil.VeloBasar.Tests;
+
+public class UserManagerMock
+    : UserManager<IdentityUser>
+{
+    public UserManagerMock()
+        : base(
+            Mock.Of<IUserStore<IdentityUser>>(),
+            Mock.Of<IOptions<IdentityOptions>>(),
+            Mock.Of<IPasswordHasher<IdentityUser>>(),
+            Enumerable.Empty<IUserValidator<IdentityUser>>(),
+            Enumerable.Empty<IPasswordValidator<IdentityUser>>(),
+            Mock.Of<ILookupNormalizer>(),
+            new IdentityErrorDescriber(),
+            Mock.Of<IServiceProvider>(),
+            Mock.Of<ILogger<UserManager<IdentityUser>>>())
+    {
+    }
+}
+
