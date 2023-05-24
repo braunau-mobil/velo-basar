8228d1407603b08863c35085e7b2710f5956f3b6
Alexander Kastler <github@xaka.eu>
2023-05-24 19:12:45 +0200

    Add CurrentThemeCookie tests

---
+++ test/BraunauMobil.VeloBasar.Tests/Cookies/CurrentThemeCookieTests/GetCurrentTheme.cs
@@ -0,0 +1,46 @@
+﻿using BraunauMobil.VeloBasar.Cookies;
+using BraunauMobil.VeloBasar.Rendering;
+
+namespace BraunauMobil.VeloBasar.Tests.Cookies.CurrentThemeCookieTests;
+
+public class GetCurrentTheme
+    : TestBase
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [AutoData]
+    public void ReturnsDefault(string cookieValue)
+    {
+        //  Arrange
+        CurrentThemeCookie sut = new (HttpContextAccessor.Object);
+        RequestCookies.SetupGet(_ => _[sut.Key])
+            .Returns(cookieValue);
+
+        //  Act
+        Theme theme = sut.GetCurrentTheme();
+
+        //  Asert
+        theme.Should().Be(Theme.DefaultLight);
+        RequestCookies.Verify(_ => _[sut.Key], Times.Once());
+        VerifyNoOtherCalls();
+    }
+
+    [Theory]
+    [InlineData("Brutal")]
+    public void ReturnsTheme(string cookieValue)
+    {
+        //  Arrange
+        CurrentThemeCookie sut = new(HttpContextAccessor.Object);
+        RequestCookies.SetupGet(_ => _[sut.Key])
+            .Returns(cookieValue);
+
+        //  Act
+        Theme theme = sut.GetCurrentTheme();
+
+        //  Asert
+        theme.Should().Be(Theme.Brutal);
+        RequestCookies.Verify(_ => _[sut.Key], Times.Once());
+        VerifyNoOtherCalls();
+    }
+}
---
+++ test/BraunauMobil.VeloBasar.Tests/Cookies/CurrentThemeCookieTests/SetCurrentTheme.cs
@@ -0,0 +1,22 @@
+﻿using BraunauMobil.VeloBasar.Cookies;
+using BraunauMobil.VeloBasar.Rendering;
+
+namespace BraunauMobil.VeloBasar.Tests.Cookies.CurrentThemeCookieTests;
+
+public class SetCurrentTheme
+    : TestBase
+{
+    [Theory]
+    [AutoData]
+    public void CallsAppend(Theme theme)
+    {
+        //  Arrange
+        CurrentThemeCookie sut = new (HttpContextAccessor.Object);
+
+        //  Act
+        sut.SetCurrentTheme(theme);
+
+        //  Asert
+        ResponseCookies.Verify(_ => _.Append(sut.Key, $"{theme}", sut.Options), Times.Once());
+    }
+}
