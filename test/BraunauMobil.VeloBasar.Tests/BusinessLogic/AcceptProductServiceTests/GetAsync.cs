bd69e9e2414cc558a41d03709207f8b6ea7aaef5
Alexander Kastler <github@xaka.eu>
2023-05-27 17:38:43 +0200

    Add tests for AcceptProductService GetAsync and UpdateAsync

--- test/BraunauMobil.VeloBasar.Tests/BusinessLogic/AcceptProductServiceTests/GetAsync.cs
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/AcceptProductServiceTests/GetAsync.cs
@@ -5,7 +5,7 @@ public class GetAsync
 {
     [Theory]
     [AutoData]
-    public async Task ProductIsReturnedAndRelationsAreLoaded(ProductEntity product)
+    public async Task WithId_ProductIsReturnedAndRelationsAreLoaded(ProductEntity product)
     {
         // Arrange
         Db.Products.Add(product);
@@ -18,6 +18,25 @@ public class GetAsync
         result.Should().NotBeNull();
         result.CanAccept.Should().BeTrue();
         result.Entity.Should().BeEquivalentTo(product);
+        result.Entity.Brand.Should().NotBeNull();
+        result.Entity.Type.Should().NotBeNull();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task WithSessionIdAndProduct_SessionIsLoadedAndProductIsReturned(ProductEntity product)
+    {
+        // Arrange
+        Db.Products.Add(product);
+        await Db.SaveChangesAsync();
+
+        // Act
+        AcceptProductModel result = await Sut.GetAsync(product.Session.Id, product);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.CanAccept.Should().BeTrue();
+        result.Entity.Should().Be(product);
         result.Entity.Brand.Should().NotBeNull();
         result.Entity.Type.Should().NotBeNull();
     }
---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/AcceptProductServiceTests/UpdateAsync.cs
@@ -0,0 +1,36 @@
+﻿using Xan.AspNetCore.EntityFrameworkCore;
+
+namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.AcceptProductServiceTests;
+
+
+public class UpdateAsync
+	: TestBase
+{
+	[Theory]
+	[AutoData]
+	public async Task ProductIsAttachedAndUpdated(ProductEntity productToInsert, BrandEntity brand, ProductTypeEntity productType, ProductEntity productToUpdate)
+	{
+		//	Arrange
+		Db.Products.Add(productToInsert);
+		Db.Brands.Add(brand);
+		Db.ProductTypes.Add(productType);
+		await Db.SaveChangesAsync();
+		Db.ChangeTracker.Clear();
+
+		productToUpdate.Id = productToInsert.Id;
+		productToUpdate.BrandId = brand.Id;
+		productToUpdate.Brand = null!;
+		productToUpdate.TypeId = productType.Id;
+		productToUpdate.Type = null!;
+		productToUpdate.SessionId = productToInsert.SessionId;
+		productToUpdate.Session = null!;
+
+		//	Act
+		await Sut.UpdateAsync(productToUpdate);
+
+		//	Assert
+		ProductEntity updatedProduct = await Db.Products.FirstByIdAsync(productToInsert.Id);
+		updatedProduct.Should().BeEquivalentTo(productToUpdate);
+	}
+}
+
