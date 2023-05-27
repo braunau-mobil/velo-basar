a0365c94ff243443d9650b97a2c1ae31080716cf
Alexander Kastler <github@xaka.eu>
2023-05-27 17:47:35 +0200

    Add test for AcceptSessionService CreateAsync

---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/AcceptSessionServiceTests/CreateAsync.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.AcceptSessionServiceTests;
+
+public class CreateAsync
+	: TestBase
+{
+	[Theory]
+	[AutoData]
+	public async Task AcceptSessionIsCreatedAndInserted(BasarEntity basar, SellerEntity seller, DateTime dateTime)
+	{
+		//	Arrange
+		Db.Basars.Add(basar);
+		Db.Sellers.Add(seller);
+		await Db.SaveChangesAsync();
+
+		Clock.Setup(_ => _.GetCurrentDateTime())
+			.Returns(dateTime);
+
+		//	Act
+		AcceptSessionEntity acceptSession = await Sut.CreateAsync(basar.Id, seller.Id);
+
+		//	Assert
+		acceptSession.Should().NotBeNull();
+		acceptSession.StartTimeStamp.Should().Be(dateTime);
+		acceptSession.Seller.Should().Be(seller);
+		acceptSession.SellerId.Should().Be(seller.Id);
+		acceptSession.Basar.Should().Be(basar);
+		acceptSession.BasarId.Should().Be(basar.Id);
+
+		VerifyNoOtherCalls();
+	}
+}
+
---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/AcceptSessionServiceTests/TestBase.cs
@@ -0,0 +1,21 @@
+﻿using BraunauMobil.VeloBasar.BusinessLogic;
+
+namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.AcceptSessionServiceTests;
+
+public class TestBase
+    : SqliteTestBase
+{
+    public TestBase()
+    {
+        Sut = new AcceptSessionService(Db, TransactionService.Object, Clock.Object);
+    }
+
+    public void VerifyNoOtherCalls()
+    {
+        TransactionService.VerifyNoOtherCalls();
+    }
+    
+    public AcceptSessionService Sut { get; }
+
+    public Mock<ITransactionService> TransactionService { get; } = new();
+}
