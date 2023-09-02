41bc5d8f21f9c7f6f90714faa2cb0c1ed468dc4a
Alexander Kastler <github@xaka.eu>
2023-09-02 11:42:54 +0200

    Add Tests for IsSessionRunning and Submit for AcceptSessionService

---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/AcceptSessionServiceTests/IsSessionRunning.cs
@@ -0,0 +1,62 @@
+﻿namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.AcceptSessionServiceTests;
+
+public class IsSessionRunning
+    : TestBase
+{
+    [Fact]
+    public async Task Null_ReturnsFalse()
+    {
+        //  Arrange
+
+        //  Act
+        bool isRunning = await Sut.IsSessionRunning(null);
+
+        //  Assert
+        isRunning.Should().BeFalse();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task NonExistent_ReturnsFalse(int sessionId)
+    {
+        // Arrange
+
+        //  Act
+        bool isRunning = await Sut.IsSessionRunning(sessionId);
+
+        //  Assert
+        isRunning.Should().BeFalse();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task EndTimeStampSet_ReturnsFalse(AcceptSessionEntity session, DateTime endTimeStamp)
+    {
+        // Arrange
+        Db.AcceptSessions.Add(session);
+        session.EndTimeStamp = endTimeStamp;
+        await Db.SaveChangesAsync();
+
+        //  Act
+        bool isRunning = await Sut.IsSessionRunning(session.Id);
+
+        //  Assert
+        isRunning.Should().BeFalse();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task NoEndTimeStamp_ReturnsTrue(AcceptSessionEntity session)
+    {
+        // Arrange
+        Db.AcceptSessions.Add(session);
+        session.EndTimeStamp = null;
+        await Db.SaveChangesAsync();
+
+        //  Act
+        bool isRunning = await Sut.IsSessionRunning(session.Id);
+
+        //  Assert
+        isRunning.Should().BeTrue();
+    }
+}
---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/AcceptSessionServiceTests/SubmitAsync.cs
@@ -0,0 +1,32 @@
+﻿namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.AcceptSessionServiceTests;
+
+public class SubmitAsync
+    : TestBase
+{
+    [Theory]
+    [AutoData]
+    public async Task SessionIsCompletedAndAcceptIsCalled(AcceptSessionEntity session, DateTime endTimeStamp, int acceptanceId)
+    {
+        // Arrange
+        session.State = AcceptSessionState.Uncompleted;
+        session.EndTimeStamp = null;
+        session.Seller.ValueState = ValueState.Settled;
+        Db.AcceptSessions.Add(session);
+        await Db.SaveChangesAsync();
+        TransactionService.Setup(_ => _.AcceptAsync(session.BasarId, session.SellerId, Enumerable.Empty<int>()))
+            .ReturnsAsync(acceptanceId);
+        Clock.Setup(_ => _.GetCurrentDateTime())
+            .Returns(endTimeStamp);
+
+
+        //  Act
+        int result= await Sut.SubmitAsync(session.Id);
+
+        //  Assert
+        result.Should().Be(acceptanceId);
+        session.State.Should().Be(AcceptSessionState.Completed);
+        session.EndTimeStamp.Should().Be(endTimeStamp);
+        session.Seller.ValueState.Should().Be(ValueState.NotSettled);
+        TransactionService.Verify(_ => _.AcceptAsync(session.BasarId, session.SellerId, Enumerable.Empty<int>()), Times.Once());
+    }
+}
