8321819f400431aadcaa8b4f52fa9e20663565ba
Alexander Kastler <github@xaka.eu>
2023-09-10 08:44:12 +0200

    Add test for ColorProvider

---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/ColorProviderTests/Indexer.cs
@@ -0,0 +1,22 @@
+﻿using BraunauMobil.VeloBasar.BusinessLogic;
+using Xan.Extensions;
+
+namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.ColorProviderTests;
+
+public class Indexer
+{
+    [Theory]
+    [AutoData]
+    public void ReturnsAlwaysTheSame(string value)
+    {
+        //  Arrange
+        ColorProvider sut = new();
+
+        //  Act
+        Color c1 = sut[value];
+        Color c2 = sut[value];
+
+        //  Assert
+        c1.Should().Be(c2);
+    }
+}
