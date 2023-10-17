20f0167f684c9c15528468f18ea96a7d35289ff5
Alexander Kastler <github@xaka.eu>
2023-10-17 14:30:03 +0200

    Add test for ProductService GetLabelAsync

---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/ProductServiceTests/GetLabelAsync.cs
@@ -0,0 +1,38 @@
+﻿namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.ProductServiceTests;
+
+public class GetLabelAsync
+    : TestBase
+{
+    [Theory]
+    [AutoData]
+    public void EmptyDatabase_Throws(int productId)
+    {
+        //  Arrange
+
+        //  Act
+        Func<Task> act = async () => await Sut.GetLabelAsync(productId);
+
+        //  Assert
+        act.Should().ThrowAsync<InvalidOperationException>();
+    }
+
+    [Theory]
+    [AutoData]
+    public async void ProductExists_ReturnsLabelDataAsPdf(ProductEntity product, byte[] labelData)
+    {
+        //  Arrange
+        product.Id = 1;
+        Db.Products.Add(product);
+        await Db.SaveChangesAsync();
+        ProductLabelService.Setup(_ => _.CreateLabelAsync(product))
+            .ReturnsAsync(labelData);
+
+        //  Act
+        FileDataEntity fileData = await Sut.GetLabelAsync(product.Id);
+        
+        //  Assert
+        fileData.ContentType.Should().Be(FileDataEntity.PdfContentType);
+        fileData.Data.Should().BeEquivalentTo(labelData);
+        fileData.FileName.Should().Be("Product-1_Label.pdf");
+    }
+}
