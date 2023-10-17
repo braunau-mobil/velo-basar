ab06349dc4a412803172c947dffcf6e4720b6edc
Alexander Kastler <github@xaka.eu>
2023-10-17 14:35:31 +0200

    Add test for ProductService GetManyAsync

---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/ProductServiceTests/GetManyAsync_ListOfIds.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.ProductServiceTests;
+
+public class GetManyAsync_ListOfIds
+    : TestBase
+{
+    [Theory]
+    [AutoData]
+    public async Task EmptyDatabase_ReturnsEmptyList(int[] productIds)
+    {
+        //  Arrange
+
+        //  Act
+        IReadOnlyList<ProductEntity> products = await Sut.GetManyAsync(productIds);
+
+        //  Assert
+        products.Should().BeEmpty();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task ProductExists_ReturnsListOfProducts(ProductEntity p1, ProductEntity p2, ProductEntity p3, int productIdThatDoesNotExist)
+    {
+        //  Arrange
+        Db.Products.AddRange(p1, p2, p3);
+        await Db.SaveChangesAsync();
+        int[] ids = new []
+        {
+            p1.Id,
+            p2.Id,
+            p3.Id,
+            productIdThatDoesNotExist
+        };
+
+        //  Act
+        IReadOnlyCollection<ProductEntity> products = await Sut.GetManyAsync(ids);
+
+        //  Assert
+        products.Should().HaveCount(3);
+        products.Should().ContainEquivalentOf(p1);
+        products.Should().ContainEquivalentOf(p2);
+        products.Should().ContainEquivalentOf(p3);
+    }
+}
