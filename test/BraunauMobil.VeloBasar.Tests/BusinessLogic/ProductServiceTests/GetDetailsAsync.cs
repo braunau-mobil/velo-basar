52f8468debfde28a3fd3fc982183644cb30d10e5
Alexander Kastler <github@xaka.eu>
2023-10-17 14:24:34 +0200

    Add test for ProductService GetDetailsAsync

---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/ProductServiceTests/GetDetailsAsync.cs
@@ -0,0 +1,70 @@
+﻿namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.ProductServiceTests;
+
+public class GetDetailsAsync
+    : TestBase
+{
+    [Theory]
+    [AutoData]
+    public async Task EmptyDatabase_Throws(int basarId, int productId)
+    {
+        //  Arrange
+
+        //  Act
+        Func<Task> act = async () => await Sut.GetDetailsAsync(basarId, productId);
+
+        //  Assert
+        await act.Should().ThrowAsync<InvalidOperationException>();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task ProductExistsWithNoTransactions_ReturnsDetailsModel(ProductEntity product)
+    {
+        //  Arrange
+        Db.Products.Add(product);
+        await Db.SaveChangesAsync();
+
+        //  Act
+        ProductDetailsModel model = await Sut.GetDetailsAsync(product.Session.BasarId, product.Id);
+
+        //  Assert
+        model.Entity.Should().BeEquivalentTo(product);
+        model.Transactions.Should().BeEmpty();
+        model.CanLock.Should().BeFalse();
+        model.CanSetAsLost.Should().BeFalse();
+        model.CanUnlock.Should().BeFalse();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task ProductAndTransactionsExist_ReturnsDetailsModel(ProductEntity product)
+    {
+        //  Arrange
+        Db.Products.Add(product);
+        await Db.SaveChangesAsync();
+        TransactionEntity transaction = new ()
+        {
+            Basar = product.Session.Basar,
+            Type = TransactionType.Acceptance,
+            Seller = product.Session.Seller
+        };
+        transaction.Products.Clear();
+        transaction.Products.Add(new ProductToTransactionEntity
+        {
+            Transaction = transaction,
+            Product = product
+        });
+        Db.Transactions.Add(transaction);
+        await Db.SaveChangesAsync();
+
+        //  Act
+        ProductDetailsModel model = await Sut.GetDetailsAsync(product.Session.BasarId, product.Id);
+
+        //  Assert
+        model.Entity.Should().BeEquivalentTo(product);
+        model.Transactions.Should().ContainEquivalentOf(transaction);
+        model.CanLock.Should().BeFalse();
+        model.CanSetAsLost.Should().BeFalse();
+        model.CanUnlock.Should().BeFalse();
+    }
+}
