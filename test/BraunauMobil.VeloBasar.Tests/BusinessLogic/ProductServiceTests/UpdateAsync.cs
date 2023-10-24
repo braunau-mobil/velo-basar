c25368694b51ffcf23dd4746bce6f2fca0228a03
Alexander Kastler <github@xaka.eu>
2023-10-24 09:03:19 +0200

    Add test for ProductService UpdateAsync

---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/ProductServiceTests/UpdateAsync.cs
@@ -0,0 +1,24 @@
+﻿using Xan.AspNetCore.EntityFrameworkCore;
+
+namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.ProductServiceTests;
+
+public class UpdateAsync
+    : TestBase
+{
+    [Theory]
+    [AutoData]
+    public async Task ProductGetsUpdated(ProductEntity product, string newDescription)
+    {
+        //  Arrange
+        Db.Products.Add(product);
+        await Db.SaveChangesAsync();
+
+        //  Act
+        product.Description = newDescription;
+        await Sut.UpdateAsync(product);
+
+        //  Assert
+        ProductEntity updatedProduct = await Db.Products.FirstByIdAsync(product.Id);
+        updatedProduct.Should().BeEquivalentTo(product);
+    }
+}
