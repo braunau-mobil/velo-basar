aac4acf1a7f2947b6d9d1d1e4cc9b293ea4ca06f
Alexander Kastler <github@xaka.eu>
2023-10-24 09:32:35 +0200

    Add tests for SellerService ExistsAsync

---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/SellerServiceTests/ExistsAsync.cs
@@ -0,0 +1,33 @@
+﻿namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.SellerServiceTests;
+
+public class ExistsAsync
+    : TestBase
+{
+    [Theory]
+    [AutoData]
+    public async Task DoesNotExist_ReturnsFalse(int sellerId)
+    {
+        //  Arrange
+
+        //  Act
+        bool result = await Sut.ExistsAsync(sellerId);
+
+        //  Assert
+        result.Should().BeFalse();
+    }
+
+    [Theory]
+    [AutoData]
+    public async Task Exists_ReturnsTrue(SellerEntity seller)
+    {
+        //  Arrange
+        Db.Sellers.Add(seller);
+        await Db.SaveChangesAsync();
+
+        //  Act
+        bool result = await Sut.ExistsAsync(seller.Id);
+
+        //  Assert
+        result.Should().BeTrue();
+    }
+}
---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/SellerServiceTests/TestBase.cs
@@ -0,0 +1,34 @@
+﻿using BraunauMobil.VeloBasar.BusinessLogic;
+using BraunauMobil.VeloBasar.Pdf;
+
+namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.SellerServiceTests;
+
+public class TestBase
+    : SqliteTestBase
+{
+    public TestBase()
+    {
+        Sut = new SellerService(TransactionService.Object, ProductLabelService.Object, TokenProvider.Object, Clock.Object, Db);
+    }
+
+    public override void Dispose()
+    {
+        VerifyNoOtherCalls();
+        base.Dispose();
+    }
+
+    public void VerifyNoOtherCalls()
+    {
+        ProductLabelService.VerifyNoOtherCalls();
+        TokenProvider.VerifyNoOtherCalls();
+        TransactionService.VerifyNoOtherCalls();
+    }
+    
+    public SellerService Sut { get; }
+
+    public Mock<IProductLabelService> ProductLabelService { get; } = new();
+
+    public Mock<ITokenProvider> TokenProvider { get; } = new ();
+
+    public Mock<ITransactionService> TransactionService { get; } = new();
+}
