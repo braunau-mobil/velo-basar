228df2a70d26bba50f26c24413e6767642861400
Alexander Kastler <github@xaka.eu>
2023-11-06 08:23:08 +0100

    Add test for SellerService GetDetailsAsync

---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/SellerServiceTests/DetailsAsync.cs
@@ -0,0 +1,28 @@
+﻿namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.SellerServiceTests;
+
+public class DetailsAsync
+    : TestBase<SampleSqliteDbFixture>
+{
+    [Theory]
+    [InlineData(1, 12)]
+    public async Task DetailsAreCorrect(int basarId, int sellerId)
+    {
+        //  Arrange
+
+        //  Act
+        SellerDetailsModel model = await Sut.GetDetailsAsync(basarId, sellerId);
+
+        //  Assert
+        model.AcceptedProductCount.Should().Be(3);
+        model.Entity.Id.Should().Be(sellerId);
+        model.Entity.ValueState.Should().Be(ValueState.NotSettled);
+        model.NotSoldProductCount.Should().Be(1);
+        model.PickedUpProductCount.Should().Be(0);
+        model.Procucts.Should().HaveCount(3);
+        model.SettlementAmout.Should().Be(181.65M);
+        model.SoldProductCount.Should().Be(2);
+        model.Transactions.Should().HaveCount(3);
+
+        VerifyNoOtherCalls();
+    }
+}
