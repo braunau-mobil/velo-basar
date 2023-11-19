36b8b05d7c8fcfda77513a5e6196ab822f08c3f9
Alexander Kastler <github@xaka.eu>
2023-11-19 17:11:38 +0100

    Add test for SimpleTokenProvider

---
+++ test/BraunauMobil.VeloBasar.Tests/BusinessLogic/SimpleTokenProviderTests/CreateToken.cs
@@ -0,0 +1,24 @@
+﻿using BraunauMobil.VeloBasar.BusinessLogic;
+
+namespace BraunauMobil.VeloBasar.Tests.BusinessLogic.SimpleTokenProviderTests;
+
+public class CreateToken
+{
+    [Theory]
+    [AutoData]
+    public void ShouldReturnFirstNameIdLastNameAsHex(SellerEntity seller)
+    {
+        //  Arrange
+        SimpleTokenProvider sut = new();
+        seller.FirstName = "Abcd";
+        seller.LastName = "xYz";
+        seller.Id = 666;
+
+        //  Act
+        string result = sut.CreateToken(seller);
+
+        //  Assert
+        result.Should().NotBeNullOrWhiteSpace();
+        result.Should().Be("416229A7859");
+    }
+}
