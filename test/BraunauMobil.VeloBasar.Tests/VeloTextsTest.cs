e531836808ad2d353e40c5d4c09bddccdbb4b30b
Alexander Kastler <github@xaka.eu>
2023-12-27 13:33:08 +0100

    Add test for missing translations #237

---
+++ test/BraunauMobil.VeloBasar.Tests/VeloTextsTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BraunauMobil.VeloBasar.Tests;
+
+public class VeloTextsTest
+{
+    private class MemoryLogger
+        : ILogger
+    {
+        public List<string> LogEntries { get; } = new ();
+
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull
+            => throw new NotImplementedException();
+
+        public bool IsEnabled(LogLevel logLevel)
+            => throw new NotImplementedException();
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            LogEntries.Add($"{logLevel}_{eventId}_{formatter(state, exception)}");
+        }
+    }
+
+    [Fact]
+    public void AllShouldBeTranslated()
+    {
+        //  Arrange
+        MemoryLogger logger = new ();
+
+        //  Act
+        VeloTexts.CheckIfAllIsTranslated(logger);
+
+        //  Assert
+        logger.LogEntries.Should().BeEmpty();
+    }
+}
