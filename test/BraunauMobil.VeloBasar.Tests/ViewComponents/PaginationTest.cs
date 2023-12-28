6af49eda475b724f280134706576d599a113edac
Alexander Kastler <github@xaka.eu>
2023-12-28 11:57:40 +0100

    Add tests for ViewComponents #237

---
+++ test/BraunauMobil.VeloBasar.Tests/ViewComponents/PaginationTest.cs
@@ -0,0 +1,35 @@
+﻿using BraunauMobil.VeloBasar.ViewComponents;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Xan.AspNetCore.Models;
+using Xan.AspNetCore.Parameter;
+using Xan.Extensions.Collections;
+
+namespace BraunauMobil.VeloBasar.Tests.ViewComponents;
+
+public class PaginationTest
+{
+    [Theory]
+    [AutoData]
+    public void ShouldReturnPaginationModel(ListParameter parameter)
+    {
+        //  Arrange
+        Pagination sut = new();
+        IPaginatedList items = A.Fake<IPaginatedList>();
+        Func<ListParameter, string> toListFunc = X.StrictFake<Func<ListParameter, string>>();
+
+        //  Act
+        IViewComponentResult result = sut.Invoke(items, parameter, toListFunc);
+
+        //  Assert
+        using (new AssertionScope())
+        {
+            ViewViewComponentResult view = result.Should().BeOfType<ViewViewComponentResult>().Subject;
+            view.ViewData.Should().NotBeNull();
+            view.ViewData!.ModelState.IsValid.Should().BeTrue();
+
+            PaginationModel model = view.ViewData.Model.Should().BeOfType<PaginationModel>().Subject;
+            model.Items.Should().Be(items);
+        }
+    }
+}
---
+++ test/BraunauMobil.VeloBasar.Tests/ViewComponents/SearchTest.cs
@@ -0,0 +1,32 @@
+﻿using BraunauMobil.VeloBasar.ViewComponents;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace BraunauMobil.VeloBasar.Tests.ViewComponents;
+
+public class SearchTest
+{
+    [Theory]
+    [InlineAutoData(null)]
+    [AutoData]
+    public void ShouldReturnSearchViewModel(string? searchString, string resetUrl)
+    {
+        //  Arrange
+        Search sut = new();
+
+        //  Act
+        IViewComponentResult result = sut.Invoke(searchString, resetUrl);
+
+        //  Assert
+        using (new AssertionScope())
+        {
+            ViewViewComponentResult view = result.Should().BeOfType<ViewViewComponentResult>().Subject;
+            view.ViewData.Should().NotBeNull();
+            view.ViewData!.ModelState.IsValid.Should().BeTrue();
+            
+            SearchModel model = view.ViewData.Model.Should().BeOfType<SearchModel>().Subject;
+            model.SearchString.Should().Be(searchString);
+            model.ResetUrl.Should().Be(resetUrl);
+        }
+    }
+}
