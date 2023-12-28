02df4524835d8daad154aeed8ccbd94b497599d9
Alexander Kastler <github@xaka.eu>
2023-12-28 19:03:53 +0100

    Add test for DefaultVeloHtmlFactory Badge #237

---
+++ test/BraunauMobil.VeloBasar.Tests/Rendering/DefaultVeloHtmlFactoryTests/Badge.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BraunauMobil.VeloBasar.Tests.Rendering.DefaultVeloHtmlFactoryTests;
+
+public class Badge
+    : TestBase
+{
+    [Fact]
+    public void ShouldReturnCorrectHtml()
+    {
+        // Arrange
+
+        // Act
+        TagBuilder actual = Sut.Badge(VeloBasar.Rendering.BadgeType.Primary);
+
+        // Assert
+        actual.Should().Html("""<span class="badge rounded-pill text-bg-primary"></span>""");
+    }
+}
