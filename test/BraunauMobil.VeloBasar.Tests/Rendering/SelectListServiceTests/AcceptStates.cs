9e292a415a324b6a6149dc133c3bbc3242d20f39
Alexander Kastler <github@xaka.eu>
2023-12-30 12:25:26 +0100

    Add tests for SelectListService #237

---
+++ test/BraunauMobil.VeloBasar.Tests/Rendering/SelectListServiceTests/AcceptStates.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BraunauMobil.VeloBasar.Tests.Rendering.SelectListServiceTests;
+
+
+public class AcceptStates
+    : TestBase
+{
+    [Fact]
+    public void DoNotIncludeAll()
+    {
+        //  Arrange
+
+        // Act
+        SelectList result = Sut.AcceptStates();
+
+        // Assert
+        result.Should().SatisfyRespectively(
+            item =>
+            {
+                item.Disabled.Should().BeFalse();
+                item.Group.Should().BeNull();
+                item.Selected.Should().BeFalse();
+                item.Text.Should().Be("VeloBasar_Uncompleted");
+                item.Value.Should().Be("Uncompleted");
+            },
+            item =>
+            {
+                item.Disabled.Should().BeFalse();
+                item.Group.Should().BeNull();
+                item.Selected.Should().BeFalse();
+                item.Text.Should().Be("VeloBasar_Completed");
+                item.Value.Should().Be("Completed");
+            }
+        );
+    }
+
+    [Fact]
+    public void IncludeAll()
+    {
+        //  Arrange
+
+        // Act
+        SelectList result = Sut.AcceptStates(includeAll: true);
+
+        // Assert
+        result.Should().SatisfyRespectively(
+            item =>
+            {
+                item.Disabled.Should().BeFalse();
+                item.Group.Should().BeNull();
+                item.Selected.Should().BeFalse();
+                item.Text.Should().Be("VeloBasar_AllStates");
+                item.Value.Should().Be("");
+            },
+            item =>
+            {
+                item.Disabled.Should().BeFalse();
+                item.Group.Should().BeNull();
+                item.Selected.Should().BeFalse();
+                item.Text.Should().Be("VeloBasar_Uncompleted");
+                item.Value.Should().Be("Uncompleted");
+            },
+            item =>
+            {
+                item.Disabled.Should().BeFalse();
+                item.Group.Should().BeNull();
+                item.Selected.Should().BeFalse();
+                item.Text.Should().Be("VeloBasar_Completed");
+                item.Value.Should().Be("Completed");
+            }
+        );
+    }
+}
---
+++ test/BraunauMobil.VeloBasar.Tests/Rendering/SelectListServiceTests/TestBase.cs
@@ -0,0 +1,14 @@
+﻿using BraunauMobil.VeloBasar.Rendering;
+
+namespace BraunauMobil.VeloBasar.Tests.Rendering.SelectListServiceTests;
+
+public class TestBase
+    : DbTestBase<EmptySqliteDbFixture>
+{
+    public TestBase()
+    {
+        Sut = new SelectListService(Db, new StringLocalizerMock<SharedResources>());
+    }
+
+    public SelectListService Sut { get; }
+}
