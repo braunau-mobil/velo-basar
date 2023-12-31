2604d46796c8bb66c64fade5d2cca71a070cc19f
Alexander Kastler <github@xaka.eu>
2023-12-31 09:47:46 +0100

    Add tests for AcceptanceLabelsRouter #237

---
+++ test/BraunauMobil.VeloBasar.Tests/Routing/AcceptanceLabelsRouterTests.cs
@@ -0,0 +1,37 @@
+﻿using BraunauMobil.VeloBasar.Routing;
+
+namespace BraunauMobil.VeloBasar.Tests.Routing;
+
+public class AcceptanceLabelsRouterTests
+{
+    private readonly AcceptanceLabelsRouter _sut;
+
+    public AcceptanceLabelsRouterTests()
+    {
+        _sut = new AcceptanceLabelsRouter(new LinkGeneratorMock());
+    }
+
+    [Fact]
+    public void ToDownload()
+    {
+        //  Arrange
+
+        //  Act
+        string result = _sut.ToDownload(666);
+
+        //  Assert
+        result.Should().Be("//id=666&action=Download&controller=AcceptanceLabels");
+    }
+
+    [Fact]
+    public void ToSelect()
+    {
+        //  Arrange
+
+        //  Act
+        string result = _sut.ToSelect();
+
+        //  Assert
+        result.Should().Be("//action=Select&controller=AcceptanceLabels");
+    }
+}
