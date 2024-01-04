84da2cf7bd6f5f9b8ada10f97a850dea141a896b
Alexander Kastler <github@xaka.eu>
2024-01-04 12:53:56 +0100

    Remove warning in tests #237

--- test/BraunauMobil.VeloBasar.Tests/ViewComponents/SearchTest.cs
+++ test/BraunauMobil.VeloBasar.Tests/ViewComponents/SearchTest.cs
@@ -7,9 +7,16 @@ namespace BraunauMobil.VeloBasar.Tests.ViewComponents;
 public class SearchTest
 {
     [Theory]
-    [VeloInlineAutoData(null)]
     [VeloAutoData]
     public void ShouldReturnSearchViewModel(string? searchString, string resetUrl)
+        => RunTest(searchString, resetUrl);
+
+    [Theory]
+    [VeloAutoData]
+    public void SearchStringIsNull_ShouldReturnSearchViewModel(string resetUrl)
+        => RunTest(null, resetUrl);
+
+    private static void RunTest(string? searchString, string resetUrl)
     {
         //  Arrange
         Search sut = new();
@@ -23,7 +30,7 @@ public class SearchTest
             ViewViewComponentResult view = result.Should().BeOfType<ViewViewComponentResult>().Subject;
             view.ViewData.Should().NotBeNull();
             view.ViewData!.ModelState.IsValid.Should().BeTrue();
-            
+
             SearchModel model = view.ViewData.Model.Should().BeOfType<SearchModel>().Subject;
             model.SearchString.Should().Be(searchString);
             model.ResetUrl.Should().Be(resetUrl);
